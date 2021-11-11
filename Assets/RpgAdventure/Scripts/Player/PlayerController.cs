ab1b2642f01d219a888d1f255f445348b38ad82e
Ambi Sho <ambi.sho@woven-planet.global>
2021-11-11 17:54:41 +0900

    Controlling forward speed

--- Assets/RpgAdventure/Scripts/Player/PlayerController.cs
+++ Assets/RpgAdventure/Scripts/Player/PlayerController.cs
@@ -7,13 +7,16 @@ namespace RpgAdventure
 
     public class PlayerController : MonoBehaviour
     {
-        public float speed;
+        public float maxForwardSpeed = 8.0f;
         public float rotationSpeed;
 
         private PlayerInput m_PlayerInput;
         private CharacterController m_ChController;
         private Animator m_Animator;
         private Camera m_MainCamera;
+
+        private float m_DesiredForwardSpeed;
+        private float m_ForwardSpeed;
 
         private readonly int m_HashForwardSpeed = Animator.StringToHash("ForwardSpeed");
 
@@ -24,18 +27,25 @@ namespace RpgAdventure
             m_Animator = GetComponent<Animator>();
             m_MainCamera = Camera.main;
         }
-        void FixedUpdate()
+        private void FixedUpdate()
         {
-            Vector3 moveInput = m_PlayerInput.MoveInput;
-            Quaternion camRotation = m_MainCamera.transform.rotation;
-            Vector3 targetDirection = camRotation * moveInput;
-            targetDirection.y = 0;
+            ComputeMovement();
 
-            m_ChController.Move(targetDirection.normalized * speed * Time.fixedDeltaTime);
-            m_ChController.transform.rotation = Quaternion.Euler(0, camRotation.eulerAngles.y, 0);
+        }
 
-            m_Animator.SetFloat("m_HashForwardSpeed", moveInput.magnitude);
+        private void ComputeMovement()
+        {
+            Vector3 moveInput = m_PlayerInput.MoveInput.normalized;
+            m_DesiredForwardSpeed = moveInput.magnitude * maxForwardSpeed;
 
+            m_ForwardSpeed = Mathf.MoveTowards(
+                m_ForwardSpeed,
+                m_DesiredForwardSpeed,
+                Time.fixedDeltaTime);
+
+           
+
+            m_Animator.SetFloat(m_HashForwardSpeed , m_ForwardSpeed);
         }
     }
 }
