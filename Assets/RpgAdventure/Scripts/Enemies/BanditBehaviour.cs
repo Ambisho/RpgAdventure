89944ba5675428849fc7824a4470e08bda8b85be
Ambi Sho <ambi.sho@woven-planet.global>
2021-11-12 14:55:16 +0900

    Detection Range

--- Assets/RpgAdventure/Scripts/Enemies/BanditBehaviour.cs
+++ Assets/RpgAdventure/Scripts/Enemies/BanditBehaviour.cs
@@ -6,8 +6,39 @@ namespace RpgAdventure
 {
     public class BanditBehaviour : MonoBehaviour
     {
+        public float detedtionRadius = 10.0f;
 
+        private void Update()
+        {
+            LookForPlayer();
+        }
+
+        private PlayerController LookForPlayer()
+        {
+            if (PlayerController.Instance == null)
+            {
+                return null;
+            }
+
+            Vector3 enemyPosition = transform.position;
+            Vector3 toPlayer = PlayerController.Instance.transform.position - enemyPosition;
+            toPlayer.y = 0;
+
+            if (toPlayer.magnitude <= detedtionRadius)
+            {
+                Debug.Log("Detecting the player!");
+            }
+            else
+            {
+                Debug.Log("Where are you?");
+            }
+
+            return null;
+        }
     }
+
+
+
 }
 
 
