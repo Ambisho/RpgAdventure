41b56eff1291237cf887a2e06114fb52a64adbcf
Ambi Sho <ambi.sho@woven-planet.global>
2021-11-16 10:05:27 +0900

    Get player position

--- Assets/RpgAdventure/Scripts/Enemies/BanditBehaviour.cs
+++ Assets/RpgAdventure/Scripts/Enemies/BanditBehaviour.cs
@@ -6,11 +6,18 @@ namespace RpgAdventure
 {
     public class BanditBehaviour : MonoBehaviour
     {
-        public float detedtionRadius = 10.0f;
+        public float detectionRadius = 10.0f;
+        public float detectionAngle = 90.0f;
 
+        private PlayerController m_Target;
         private void Update()
         {
-            LookForPlayer();
+            m_Target = LookForPlayer();
+
+            if (!m_Target) { return; }
+
+            Vector3 targetPosition = m_Target.transform.position;
+            Debug.Log(targetPosition);
         }
 
         private PlayerController LookForPlayer()
@@ -24,20 +31,38 @@ namespace RpgAdventure
             Vector3 toPlayer = PlayerController.Instance.transform.position - enemyPosition;
             toPlayer.y = 0;
 
-            if (toPlayer.magnitude <= detedtionRadius)
+            if (toPlayer.magnitude <= detectionRadius)
             {
-                Debug.Log("Detecting the player!");
+                if (Vector3.Dot(toPlayer.normalized, transform.forward) >
+                    Mathf.Cos(detectionAngle * 0.5f * Mathf.Deg2Rad))
+                {
+                    return PlayerController.Instance;
+                }
             }
-            else
-            {
-                Debug.Log("Where are you?");
-            }
+            
 
             return null;
         }
+ #if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            Color c = new Color(0, 0, 0.7f, 0.4f);
+            UnityEditor.Handles.color = c;
+
+            Vector3 rotatedForward = Quaternion.Euler(
+                0,
+                -detectionAngle * 0.5f,
+                0) * transform.forward;
+
+            UnityEditor.Handles.DrawSolidArc(
+                transform.position,
+                Vector3.up,
+                rotatedForward,
+                detectionAngle,
+                detectionRadius);
+        }
+#endif 
     }
-
-
 
 }
 
